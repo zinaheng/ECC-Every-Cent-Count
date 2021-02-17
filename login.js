660e9162cf69d4f1017121c97bbb7a292c12a351
zinaheng <Zinah31it@yahoo.com>
2021-02-17 16:03:25 -0600

    login works now
    login/signUp works now

--- login.js
+++ login.js
@@ -13,36 +13,41 @@ signInButton.addEventListener('click',() =>
 	);
 
 //  to configure firebase
-	var firebaseConfig = {
-		apiKey: "AIzaSyDQjxHBgTlS2c8SY_Oaej49ka7yaD8WcNM",
-		authDomain: "ecc-every-cent-count.firebaseapp.com",
-	   projectId: "ecc-every-cent-count",
-	   storageBucket: "ecc-every-cent-count.appspot.com",
-	   messagingSenderId: "181623300107",
-	   appId: "1:181623300107:web:7158b835cef503b57c670e"
-	   };
-		   
-	   firebase.initializeApp(firebaseConfig);
-   
-	   const auth = firebase.auth();
-	
-	   
-function signUp2(){
-	var email = document.getElementById("email2");
-	var password = document.getElementById("password2");
 
-	const promise = auth.createUserWithEmailAndPassword(email.value, password.value);
+
+// <!-- TODO: Add SDKs for Firebase products that you want to use
+    //  https://firebase.google.com/docs/web/setup#available-libraries -->
+
+
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyDMxgBVYWXt7sdUytNOH9EUJk1mlTIXw90",
+    authDomain: "ecc-8dd87.firebaseapp.com",
+    projectId: "ecc-8dd87",
+    storageBucket: "ecc-8dd87.appspot.com",
+    messagingSenderId: "1029521072538",
+    appId: "1:1029521072538:web:815f2237170ce443929a23"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  const auth = firebase.auth();
+	 
+function signUp(){
+	var email2 = document.getElementById("email2");
+	var password2 = document.getElementById("password2");
+	console.log(email2);
+
+	const promise = auth.createUserWithEmailAndPassword(email2.value, password2.value);
 	promise.catch(e => alert(e.message));
 	alert("Signed Up");
-
-
 }
 
-function signIn2(){
+function signIn(){
 	var email = document.getElementById("email");
 	var password = document.getElementById("password");
 	const promise = auth.signInWithEmailAndPassword(email.value, password.value);
 	promise.catch(e => alert(e.message));
+	alert("Signed In");
 	// should take the user to homepage
 
 }
@@ -52,15 +57,15 @@ function signIn2(){
 // 	alert("Signed out");
 // }
 
-auth.onAuthStateChanged(function(user){
+// auth.onAuthStateChanged(function(user){
 
-	if(user){
-		var email = user.email;
-		alert("Active User " + email);
-	}else{
-		// no user signed in
-		alert("Not an Acive User");
+// 	if(user){
+// 		var email = user.email;
+// 		alert("Active User " + email);
+// 	}else{
+// 		// no user signed in
+// 		alert("Not an Acive User");
 	
-	}
-});
+// 	}
+// });
 
